56f422db0f4dc3fd50e0737cd53c0089689a2a82
Renan Franca <renanfranca.dev@gmail.com>
2025-07-07 16:15:43 -0300

    fix: packages updated to server running

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import express, { Request, Response } from "express";
+import UserController from "./controllers/UserController";
 
 const app = express();
+
+app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
   res.json({ message: "Hello World" });
 });
+
+app.post("/createUser", UserController.createUser);
 
 const PORT = 8000;
 
